7497ae535edfc8dd6afd2e6522735fe4751fa4bc
Abdirahim Ahmed <abdirahimiahmed@gmail.com>
2023-01-04 20:33:19 +0300

    Fixed issue 1. changed the design of the editor file name

--- client/src/Components/Editor.tsx
+++ client/src/Components/Editor.tsx
@@ -1,27 +1,35 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FaJsSquare } from 'react-icons/fa';
 import io from 'socket.io-client';
+import { RootState } from '../app/store';
+import { useSelector } from 'react-redux';
 import CodeEditor from '@monaco-editor/react';
 
 interface Props {
+  file: string;
+  folder: string;
+}
+
+type CodeData = {
   code: string;
   file: string;
-}
-const Editor: React.FC<Props> = ({ code, file }) => {
+  folder: string;
+  language: string;
+  username: string;
+};
+
+  const user = useSelector((state: RootState) => state.user.value);
   useEffect(() => {
-    let socket = io('http://localhost:2321/', {
-      transports: ['websocket'],
-    });
-    socket.emit('message', 'Hello world');
   }, []);
   function handleEditorChange(value, event) {
-    console.log('here is the current model value:', value);
   }
 
   return (
     <div className="editor-container">
       <div className="editor-header">
-        <FaJsSquare className="file-icon" /> {file}
+        <div className="file-name-container">
+          <FaJsSquare className="file-icon" /> {file}
+        </div>
       </div>
       <CodeEditor
         height="90vh"
@@ -31,7 +39,6 @@ const Editor: React.FC<Props> = ({ code, file }) => {
         onChange={handleEditorChange}
       />
       <div className="editor-btns-container">
-        <button className="run">Run</button>
         <button className="test">Test</button>
       </div>
     </div>
